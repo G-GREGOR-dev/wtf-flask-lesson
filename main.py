7d0012005152f4656688ad882ba75cb5398e71f0
G-GREGOR-dev <goshasalikov@gmail.com>
2023-03-02 04:15:03 +0300

    fource-lesson

--- main.py
+++ main.py
@@ -16,12 +16,22 @@ def training(prof):
 @app.route('/list_prof/<list>')
 def prof(list):
     list_prof = ['инженер-исследователь', 'пилот', 'строитель',
-                'экзобиолог', 'врач', 'инженер по терраформированию',
-                'климатолог', 'специалист по радиационной защите',
-                'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения',
-                'метеоролог', 'оператор марсохода', 'киберинженер',
-                'штурман', 'пилот дронов']
-    return render_template('prof.html', list=list, proff = list_prof)
+                 'экзобиолог', 'врач', 'инженер по терраформированию',
+                 'климатолог', 'специалист по радиационной защите',
+                 'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения',
+                 'метеоролог', 'оператор марсохода', 'киберинженер',
+                 'штурман', 'пилот дронов']
+    return render_template('prof.html', list=list, proff=list_prof)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    jon = {'surname': 'Wanty', 'name': 'Mark', 'education': 'выше среднего',
+           'profession': 'штурман марсахода', 'sex': 'male',
+           'motivation': 'Всегда мечтал застрять на Марсе!', 'ready': True}
+    return render_template('auto_answer.html', surname=jon['surname'], name=jon['name'], education=jon['education'],
+                           profession=jon['profession'], sex=jon['sex'], motivation=jon['motivation'], ready=jon['ready'])
 
 
 if __name__ == '__main__':
