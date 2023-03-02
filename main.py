3b56f7acd7f06c746341f9c798087bd863077ee7
G-GREGOR-dev <goshasalikov@gmail.com>
2023-03-02 06:30:07 +0300

    fource-lesson

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class LoginForm(FlaskForm):
+    username_astr = StringField('id астронавта', validators=[DataRequired()])
+    password_astr = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    username_cap = StringField('id капитана', validators=[DataRequired()])
+    password_cap = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
 
 
 @app.route('/index/<title>')
@@ -11,6 +24,14 @@ def index(title):
 @app.route('/training/<prof>')
 def training(prof):
     return render_template('training.html', prof=prof)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Аварийный доступ', form=form)
 
 
 @app.route('/list_prof/<list>')
